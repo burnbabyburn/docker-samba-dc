dddd54ae89509a73dec894e823688d3238af0903
y <y@y.de>
2022-09-21 16:31:09 +0200

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -529,7 +529,7 @@ appFirstStart () {
   printf "DNS: Testing Dynamic DNS Updates" ; if ! samba_dnsupdate --verbose --use-samba-tool "${SAMBA_DEBUG_OPTION}" ; then printf "DNS: Testing Dynamic DNS Updates FAILED" ; exit 1 ; fi
   #Test - e.g. https://wiki.samba.org/index.php/Setting_up_Samba_as_an_Active_Directory_Domain_Controller
   printf "rpcclient: Connect as %s" "${DOMAIN_USER}" ; if ! rpcclient -cgetusername "-U${DOMAIN_USER}%${DOMAIN_PASS}" "${SAMBA_DEBUG_OPTION}" 127.0.0.1 ; then printf "rpcclient: Connect as %s FAILED" "${DOMAIN_USER}" ; exit 1 ; fi
-  printf "smbclient: Connect as %s" "${DOMAIN_USER}" ; if ! smbclient --debug-stdout "${SAMBA_DEBUG_OPTION}" -U"${DOMAIN_USER}%${DOMAIN_PASS}" -L LOCALHOST  | grep '[[:blank:]]session setup ok' ; then printf "smbclient: Connect as %s FAILED" "${DOMAIN_USER}"; exit 1 ; fi
+  printf "smbclient: Connect as %s" "${DOMAIN_USER}" ; if ! smbclient "${SAMBADAEMON_DEBUG_OPTION}" -U"${DOMAIN_USER}%${DOMAIN_PASS}" -L LOCALHOST ; then printf "smbclient: Connect as %s FAILED" "${DOMAIN_USER}"; exit 1 ; fi
   printf "Kerberos: Connect as %s" "${DOMAIN_USER}" ; if printf "%s" "${DOMAIN_PASS}" | kinit -V "${DOMAIN_USER}" ; then printf 'OK' ; klist ; kdestroy ; else printf "Kerberos: Connect as %s FAILED" "${DOMAIN_USER}" ; exit 1 ; fi
   echo "NTP: Check timesource and sync"; if ! chronyc sources || ! chronyc tracking; then printf "NTP: Check timesource and sync FAILED"; exit 1 ; fi
   printf "DNS: Check _ldap._tcp.%s" "${LDOMAIN}"; if ! host -t SRV _ldap._tcp."${LDOMAIN}"; then printf "DNS: Check _ldap._tcp.%s FAILED" "${LDOMAIN}"; exit 1 ; fi
